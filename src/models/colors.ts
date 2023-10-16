253e2bb9ff6b5e8071f7ce88e21645171938baaa
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-16 20:01:43 +0200

    Pol Gubau; MW-PSA

---
+++ src/models/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  background: "#e7edf4",
+  primary: "#000000",
+  secondary: "#414757",
+  error: "#f13a59",
+  success: "#00B386",
+  accent: "#4895ef",
+};
---
+++ src/models/routes.ts
@@ -0,0 +1,5 @@
+export enum ROUTES {
+  HOME = "/",
+  ABOUT = "/about",
+  
+}
