9e653278c94f9bd1d6032438915049213d53463b
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-17 16:11:58 +0200

    Pol Gubau; MW-

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind()],
+  site: "https://PolGubau.github.io/portfolio/",
 });
