6957c826ef94eacc27dfbff1665b687fed9cbe83
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-17 11:03:13 +0200

    Pol Gubau; MW-

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "static",
-  adapter: vercelStatic(),
+  site: "https://astronaut.github.io",
+  base: "/my-repo",
 });
