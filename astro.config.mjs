8309e378853ebaf7d1eee66c4460b185d6a84a30
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-17 15:50:22 +0200

    Pol Gubau; MW-New icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: "hybrid",
-  adapter: netlify(),
 });
