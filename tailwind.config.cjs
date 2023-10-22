accba423ef4b028986a709f665b5a180bfa9112a
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-22 11:59:46 +0200

    Pol Gubau; MW-Changing layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Poppins", "Inter", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         background: {
           DEFAULT: "#e7edf4",
           dark: "#1e1e1e",
         },
         primary: {
-          DEFAULT: "#c8eeff",
-          dark: "#5dccfe",
+          DEFAULT: "#ffff60",
+          dark: "#ff1",
         },
         accent: {
           DEFAULT: "#ff4",
